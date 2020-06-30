7ba88e3bfd7dcc22c22bc19cf9c35669664a36ad
greekZorba <smiling.jinhak@gmail.com>
2020-07-01 08:56:35 +0900

    강의 마무리

--- src/main/java/com/zorba/study/annotationProcessor/App.java
+++ src/main/java/com/zorba/study/annotationProcessor/App.java
@@ -3,7 +3,7 @@ package com.zorba.study.annotationProcessor;
 public class App {
 
     public static void main(String[] args) {
-        
+
 //        HatSpecificClass hatSpecificClass = new HatSpecificClass();
 //        Hat hat = new MagicHat();
 //        System.out.println(hat.pullOut());
